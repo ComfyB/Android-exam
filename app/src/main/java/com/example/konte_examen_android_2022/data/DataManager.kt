650c940d9c16f13b869018cb79db1cd47bb48d18
Christoffer Lehre <chlehre@gmail.com>
2022-08-12 18:10:09 +0200

    getting somewhere -layout done ish

--- app/src/main/java/com/example/konte_examen_android_2022/data/DataManager.kt
+++ app/src/main/java/com/example/konte_examen_android_2022/data/DataManager.kt
@@ -8,6 +8,14 @@ class DataManager {
     fun getTodoForToday(): List<ToDoItem> {
         return listOf(
             ToDoItem(R.string.ToDo1, R.drawable.important_icon_64),
+            ToDoItem(R.string.ToDo2, R.drawable.neutral_icon_64),
+            ToDoItem(R.string.ToDo3, R.drawable.important_icon_64),
+            ToDoItem(R.string.ToDo4, R.drawable.not_important_icon_64),
+            ToDoItem(R.string.ToDo5, R.drawable.important_icon_64),
+            ToDoItem(R.string.ToDo6, R.drawable.neutral_icon_64),
+            ToDoItem(R.string.ToDo7, R.drawable.important_icon_64),
+            ToDoItem(R.string.ToDo1, R.drawable.important_icon_64),
+            ToDoItem(R.string.ToDo3, R.drawable.important_icon_64),
             ToDoItem(R.string.ToDo2, R.drawable.important_icon_64)
         )
     }
